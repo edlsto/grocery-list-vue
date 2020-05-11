bd20f71bc99b3c6a0d9d5c463c9b2926c86e8f22
Edward Stoner <edwardlstoner@gmail.com>
2020-05-10 20:18:41 -0600

    Add styles for input

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="form">
-    <div class="title">
+    <div class="title input-item">
       <p>Title</p>
       <input type="text" v-model="title" />
     </div>
-    <div class="description">
+    <div class="description input-item">
       <p>Description</p>
       <input type="text" v-model="description" />
     </div>
@@ -37,5 +37,44 @@ export default {
 <style lang="scss" scoped>
 .item {
   border: 1px solid black;
+}
+
+input {
+  height: 50px;
+  font-size: 2em;
+  outline: none;
+  padding: 0.2em;
+  width: 100%;
+}
+
+.input-item {
+  width: 20em;
+  margin-right: 1em;
+}
+
+.title {
+  width: 15em;
+}
+
+.description {
+  width: 30em;
+}
+
+.form {
+  display: flex;
+  justify-content: center;
+}
+
+button {
+  height: 50px;
+  font-size: 16px;
+  align-self: flex-end;
+  padding: 0.5em;
+  width: 5em;
+  cursor: pointer;
+}
+
+p {
+  margin: 1em;
 }
 </style>
